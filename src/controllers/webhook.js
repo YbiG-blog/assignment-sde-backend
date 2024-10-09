ee880e73d7bb9fd2d1f6d006f08541c8013b4b3a
Yash Vardhan <yashvardhan7906@gmail.com>
2024-10-10 01:19:04 +0530

    webhook added

---
+++ src/controllers/webhook.js
@@ -0,0 +1,39 @@
+// service
+const { imageProcessingWebhookService } = require("../services/webhook");
+// utils
+const { responseMessage, responseTemplate } = require("../utils/response");
+
+const { WEBHOOK_API_KEY } = process.env;
+
+/**
+ *
+ * @returns {Object} - Response object
+ * @description Processes the image processing webhook request
+ */
+const imageProcessingWebhook = async (req, res) => {
+  try {
+    const apiKey = req.headers["x-api-key"];
+    if (apiKey !== "WEBHOOK_API_KEY") {
+      return res
+        .status(401)
+        .json(
+          await responseTemplate(false, responseMessage.unauthorizedAccess)
+        );
+    }
+    await imageProcessingWebhookService(req.body);
+    return res
+      .status(200)
+      .json(
+        await responseTemplate(true, "Webhook request processed successfully")
+      );
+  } catch (error) {
+    logError(error);
+    return res
+      .status(500)
+      .json(
+        await responseTemplate(false, responseMessage.serverError, null, error)
+      );
+  }
+};
+
+module.exports = { imageProcessingWebhook };
